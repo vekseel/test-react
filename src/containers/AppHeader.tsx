2edb0e7bd2a00b02c4e71554c4031e0508a6358a
aleksandr <alexeev.aleksandr@inbox.ru>
2023-05-24 20:46:07 +0300

    '

---
+++ src/containers/AppHeader.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import './AppHeader.css';
+import AppHeaderNavLink, { NavLink } from './AppHeaderNavLink';
+
+// define our AppHeader properties that will be passed into the component
+export type AppHeaderProps = {
+    title: string;
+    description: string;
+    links: NavLink[];
+};
+
+// create React Functional Component variable that will render our code for our header.
+// React.FC takes a type of the props that can be passed into the component
+const AppHeader: React.FC<AppHeaderProps> = React.memo(
+    ({ title, description, links }) => {
+        return (
+            <header className="app-header">
+            <section className="app-title">
+                <h1>{title}</h1>
+                <small>{description}</small>
+                </section>
+                <span className="fill-space" />
+        <section className="app-links">
+            {links &&
+            links.map((link: NavLink) => (
+                <AppHeaderNavLink
+                    label={link.label}
+        route={link.route}
+        key={link.label}
+        />
+    ))}
+        </section>
+        </header>
+    );
+    }
+);
+
+export default AppHeader;
